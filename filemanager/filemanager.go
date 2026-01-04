8e1b497ccdc56e25e781620d67df6571708a54a0
Ankur Gupta <ankur11.kmr@gmail.com>
2026-01-04 22:24:44 +0530

    Deferring Code Execution with 'defer'

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -20,6 +20,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New(("Could not open file!"))
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 	var lines []string
 
@@ -30,11 +32,9 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	err = scanner.Err()
 
 	if err != nil {
-		file.Close()
 		return nil, errors.New("Reading the file content failed.")
 	}
 
-	file.Close()
 	return lines, nil
 }
 
@@ -45,17 +45,17 @@ func (fm FileManager) WriteResult(data interface{}) error {
 		return errors.New("Failed to create JSON file!")
 	}
 
+	defer file.Close()
+
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	time.Sleep(3 * time.Second)
 
 	if err != nil {
-		file.Close()
 		return errors.New("Failed to encode the data!")
 	}
 
-	file.Close()
 	return nil
 }
 
