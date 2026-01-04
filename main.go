dbdfb3576c4f845e20e8a8f52542c146dd3cd582
Ankur Gupta <ankur11.kmr@gmail.com>
2026-01-04 16:51:51 +0530

    working with swappable struct and use interface for rescue

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
-
-	"example.com/price-tax-calculator/filemanager"
+	"example.com/price-tax-calculator/cmdmanager"
+	// "example.com/price-tax-calculator/filemanager"
 	"example.com/price-tax-calculator/prices"
 )
 
@@ -11,8 +10,9 @@ func main() {
 	taxRates := []float64{0, 0.05, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%0.f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%0.f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"example.com/price-tax-calculator/conversion"
-	"example.com/price-tax-calculator/filemanager"
+	"example.com/price-tax-calculator/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
-	IOManager         filemanager.FileManager `json:"-"`
-	TaxRate           float64                 `json:"tax_rate"`
-	InputPrices       []float64               `json:"input_prices"`
-	TaxIncludedPrices map[string]float64      `json:"tax_included_prices"`
+	IOManager         iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"input_prices"`
+	TaxIncludedPrices map[string]float64  `json:"tax_included_prices"`
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -45,9 +45,9 @@ func (job *TaxIncludedPriceJob) Process() {
 	job.IOManager.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		IOManager:   fm,
+		IOManager:   iom,
 		InputPrices: []float64{10, 40, 91},
 		TaxRate:     taxRate,
 	}
